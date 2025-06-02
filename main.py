2bcf5e7789278a5e500e290d03239d7e1cd6e65e
Suhas Gondukupi <suhas8838@gmail.com>
2025-06-02 22:09:08 +0530

    feat(encryption) : api_key encryption

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+from cryptography.fernet import Fernet
+from modules.api_encryption import EncryptApiKeys
+
+app = Flask(__name__)
+
+
+# STEP 1 : Generate a Fernet Encryption Key and Save it in .env this is one time
+key = Fernet.generate_key()
+print(key.decode())
+
+
+# STEP 2 : Encrypt Your API Key
+encrypted_api_key = EncryptApiKeys.encrypt_api_key('example@api_key_2025')
+print(encrypted_api_key)
+
+
+# STEP 3 : Create .env File and store encrypted_api_key received from step 2 
+
+
+# STEP 4 : Decrypt and Use the API Key 
+encrypted_api_key = os.getenv("SAMPLE_ENCRYPED_API")
+decrypted_api_key = EncryptApiKeys.decrypt_api_key(encrypted_api_key)
+print(decrypted_api_key)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
---
+++ modules/api_encryption.py
@@ -0,0 +1,21 @@
+import os
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+load_dotenv()  # Make sure this is called once globally (usually in main.py)
+
+class EncryptApiKeys:
+    @staticmethod
+    def encrypt_api_key(api_key):
+        encryption_key = os.getenv("FERNET_KEY")
+        cipher = Fernet(encryption_key.encode())  
+
+        encrypted_api_key = cipher.encrypt(api_key.encode())
+        return encrypted_api_key.decode()
+
+    @staticmethod
+    def decrypt_api_key(encrypted_api_key):
+        fernet_key = os.getenv("FERNET_KEY")
+        cipher = Fernet(fernet_key.encode())
+        return cipher.decrypt(encrypted_api_key.encode()).decode()
+
